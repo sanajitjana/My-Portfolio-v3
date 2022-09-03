1988fe97dd58e90af35f2e23c6136d3aec19a993
Sanajit Jana <dev.sanajitjana@gmail.com>
2022-09-04 03:27:55 +0530

    update hero section text

--- src/components/sections/hero.js
+++ src/components/sections/hero.js
@@ -1,8 +1,8 @@
-import React, { useState, useEffect } from 'react';
-import { CSSTransition, TransitionGroup } from 'react-transition-group';
-import styled from 'styled-components';
-import { navDelay, loaderDelay } from '@utils';
-import { usePrefersReducedMotion } from '@hooks';
+import React, { useState, useEffect } from "react";
+import { CSSTransition, TransitionGroup } from "react-transition-group";
+import styled from "styled-components";
+import { navDelay, loaderDelay } from "@utils";
+import { usePrefersReducedMotion } from "@hooks";
 // import { email } from '@config';
 
 const StyledHeroSection = styled.section`
@@ -59,28 +59,24 @@ const Hero = () => {
   }, []);
 
   const one = <h1>Hi, my name is</h1>;
-  const two = <h2 className="big-heading">Brittany Chiang.</h2>;
+  const two = <h2 className="big-heading">Sanajit Jana.</h2>;
   const three = <h3 className="big-heading">I build things for the web.</h3>;
   const four = (
     <>
       <p>
-        I’m a software engineer specializing in building (and occasionally designing) exceptional
-        digital experiences. Currently, I’m focused on building accessible, human-centered products
-        at{' '}
-        <a href="https://upstatement.com/" target="_blank" rel="noreferrer">
-          Upstatement
-        </a>
-        .
+        I’m a software engineer specializing in building exceptional digital
+        experiences. Currently, I’m focused on learning Java language and Data
+        structure and algorithm.
       </p>
     </>
   );
   const five = (
     <a
       className="email-link"
-      href="https://www.newline.co/courses/build-a-spotify-connected-app"
-      target="_blank"
-      rel="noreferrer">
-      Check out my course!
+      href="mailto:mail.sanajitjana@gmail.com"
+      rel="noreferrer"
+    >
+      Connect with me!
     </a>
   );
 
