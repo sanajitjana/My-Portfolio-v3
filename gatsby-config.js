cdef258bf69d6ec968a7350917c9b0673b275733
Sanajit Jana <dev.sanajitjana@gmail.com>
2022-09-04 02:46:29 +0530

    new update

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,13 +1,13 @@
-const config = require('./src/config');
+const config = require("./src/config");
 
 module.exports = {
   siteMetadata: {
-    title: 'Brittany Chiang',
+    title: "Sanajit Jana",
     description:
-      'Brittany Chiang is a software engineer who specializes in building (and occasionally designing) exceptional digital experiences.',
-    siteUrl: 'https://brittanychiang.com', // No trailing slash allowed!
-    image: '/og.png', // Path to your image you placed in the 'static' folder
-    twitterUsername: '@bchiang7',
+      "Sanajit Jana is a software engineer who specializes in building exceptional digital experiences.",
+    siteUrl: "https://sanajitjana.github.io", // No trailing slash allowed!
+    image: "/og.png", // Path to your image you placed in the 'static' folder
+    twitterUsername: "sanajitjana01"
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -20,43 +20,43 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: 'BrittanyChiang',
-        short_name: 'BrittanyChiang',
-        start_url: '/',
+        name: "sanajitjana",
+        short_name: "sanajitjana",
+        start_url: "/",
         background_color: config.colors.darkNavy,
         theme_color: config.colors.navy,
-        display: 'minimal-ui',
-        icon: 'src/images/logo.png',
-      },
+        display: "minimal-ui",
+        icon: "src/images/logo.png"
+      }
     },
     `gatsby-plugin-offline`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `images`,
-        path: `${__dirname}/src/images`,
-      },
+        path: `${__dirname}/src/images`
+      }
     },
     {
-      resolve: 'gatsby-source-filesystem',
+      resolve: "gatsby-source-filesystem",
       options: {
-        name: 'content',
-        path: `${__dirname}/content/`,
-      },
+        name: "content",
+        path: `${__dirname}/content/`
+      }
     },
     {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `posts`,
-        path: `${__dirname}/content/posts`,
-      },
+        path: `${__dirname}/content/posts`
+      }
     },
     {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `projects`,
-        path: `${__dirname}/content/projects`,
-      },
+        path: `${__dirname}/content/projects`
+      }
     },
     {
       resolve: `gatsby-transformer-remark`,
@@ -64,25 +64,25 @@ module.exports = {
         plugins: [
           {
             // https://www.gatsbyjs.org/packages/gatsby-remark-external-links
-            resolve: 'gatsby-remark-external-links',
+            resolve: "gatsby-remark-external-links",
             options: {
-              target: '_blank',
-              rel: 'nofollow noopener noreferrer',
-            },
+              target: "_blank",
+              rel: "nofollow noopener noreferrer"
+            }
           },
           {
             // https://www.gatsbyjs.org/packages/gatsby-remark-images
-            resolve: 'gatsby-remark-images',
+            resolve: "gatsby-remark-images",
             options: {
               maxWidth: 700,
               linkImagesToOriginal: true,
               quality: 90,
-              tracedSVG: { color: config.colors.green },
-            },
+              tracedSVG: { color: config.colors.green }
+            }
           },
           {
             // https://www.gatsbyjs.org/packages/gatsby-remark-code-titles/
-            resolve: 'gatsby-remark-code-titles',
+            resolve: "gatsby-remark-code-titles"
           }, // IMPORTANT: this must be ahead of other plugins that use code blocks
           {
             // https://www.gatsbyjs.org/packages/gatsby-remark-prismjs
@@ -95,7 +95,7 @@ module.exports = {
               // you may use this to prevent Prism from re-processing syntax.
               // This is an uncommon use-case though;
               // If you're unsure, it's best to use the default value.
-              classPrefix: 'language-',
+              classPrefix: "language-",
               // This is used to allow setting a language for inline code
               // (i.e. single backticks) by creating a separator.
               // This separator is a string and will do no white-space
@@ -125,35 +125,35 @@ module.exports = {
               // existing language" below.
               languageExtensions: [
                 {
-                  language: 'superscript',
-                  extend: 'javascript',
+                  language: "superscript",
+                  extend: "javascript",
                   definition: {
-                    superscript_types: /(SuperType)/,
+                    superscript_types: /(SuperType)/
                   },
                   insertBefore: {
                     function: {
-                      superscript_keywords: /(superif|superelse)/,
-                    },
-                  },
-                },
+                      superscript_keywords: /(superif|superelse)/
+                    }
+                  }
+                }
               ],
               // Customize the prompt used in shell output
               // Values below are default
               prompt: {
-                user: 'root',
-                host: 'localhost',
-                global: false,
-              },
-            },
-          },
-        ],
-      },
+                user: "root",
+                host: "localhost",
+                global: false
+              }
+            }
+          }
+        ]
+      }
     },
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        trackingId: 'UA-45666519-2',
-      },
-    },
-  ],
+        trackingId: "UA-200608746-1"
+      }
+    }
+  ]
 };
