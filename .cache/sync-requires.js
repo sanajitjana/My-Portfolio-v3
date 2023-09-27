8a5b891fc5dbea0e75e4c0084b7b3b7f841eeca3
Sanajit Jana <sanajit.jana@mplifytech.com>
2023-09-27 10:36:54 +0530

    Update

---
+++ .cache/sync-requires.js
@@ -0,0 +1,16 @@
+
+// prefer default export if available
+const preferDefault = m => (m && m.default) || m
+
+
+exports.components = {
+  "component---cache-dev-404-page-js": preferDefault(require("D:\\Sanajit-WorkSpace\\Resources\\Sanajit Jana\\My-Portfolio-v3\\.cache\\dev-404-page.js")),
+  "component---src-pages-404-js": preferDefault(require("D:\\Sanajit-WorkSpace\\Resources\\Sanajit Jana\\My-Portfolio-v3\\src\\pages\\404.js")),
+  "component---src-pages-archive-js": preferDefault(require("D:\\Sanajit-WorkSpace\\Resources\\Sanajit Jana\\My-Portfolio-v3\\src\\pages\\archive.js")),
+  "component---src-pages-index-js": preferDefault(require("D:\\Sanajit-WorkSpace\\Resources\\Sanajit Jana\\My-Portfolio-v3\\src\\pages\\index.js")),
+  "component---src-pages-pensieve-index-js": preferDefault(require("D:\\Sanajit-WorkSpace\\Resources\\Sanajit Jana\\My-Portfolio-v3\\src\\pages\\pensieve\\index.js")),
+  "component---src-pages-pensieve-tags-js": preferDefault(require("D:\\Sanajit-WorkSpace\\Resources\\Sanajit Jana\\My-Portfolio-v3\\src\\pages\\pensieve\\tags.js")),
+  "component---src-templates-post-js": preferDefault(require("D:\\Sanajit-WorkSpace\\Resources\\Sanajit Jana\\My-Portfolio-v3\\src\\templates\\post.js")),
+  "component---src-templates-tag-js": preferDefault(require("D:\\Sanajit-WorkSpace\\Resources\\Sanajit Jana\\My-Portfolio-v3\\src\\templates\\tag.js"))
+}
+
